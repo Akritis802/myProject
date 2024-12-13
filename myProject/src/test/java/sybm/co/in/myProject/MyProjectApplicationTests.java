72089359b8401a95fbb1c41b8f8c893d2d241b0e
Akriti Shrivastava <akritis802@gmail.com>
2024-12-13 13:07:17 +0530

    add files

---
+++ myProject/src/test/java/sybm/co/in/myProject/MyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package sybm.co.in.myProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
